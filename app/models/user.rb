9fbd914d97336e983d2a73fb2579b8d494422e28
MisterSenor <nathan.visak@gmail.com>
2020-02-14 10:06:46 -0600

    working on Sign Up workflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
   has_many :borrowed_workouts, through: :goals, source: :workouts
   has_secure_password
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
